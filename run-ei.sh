ab4f28bdc61e2ddd9a42d37bd3a1fd6b14274d38
Rafilx Tenfen <rafaeltenfen.rt@gmail.com>
2021-04-25 19:48:58 -0300

    load .env

--- run-ei.sh
+++ run-ei.sh
@@ -1,4 +1,10 @@
-sh env.sh
+if [ -f .env ]
+then
+  export $(cat .env | xargs)
+else
+  echo "You need to compile and deploy the getterSetter Contract"
+  exit 1
+fi
 
 external-initiator "{\"name\":\"eth-kovan-ei\",\"type\":\"ethereum\",\"url\":\"wss://kovan.infura.io/ws/v3/f2f8d283097642bc98a0284d408520b6\"}" \
  --port 8081 \
